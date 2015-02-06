4a38ec7138d6215a067b29ac9fafd51a89b1a736
Marco Vidal García <marcovidal67@gmail.com>
2015-02-06 02:17:13 +0100

    Database access

---
+++ application/app.py
@@ -0,0 +1,3 @@
+from persistence.places_db import PlacesDB
+dbaccess = PlacesDB()
+print dbaccess.get_categories()
--- dit.sql
+++ dit.sql
@@ -10,7 +10,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `dit`
 --
-CREATE DATABASE IF NOT EXISTS `dit` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+DROP DATABASE IF EXISTS `dit`;
+CREATE DATABASE `dit` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `dit`;
 GRANT ALL PRIVILEGES ON  `dit` . * TO  'dit'@'localhost' WITH GRANT OPTION ;
 -- --------------------------------------------------------
@@ -34,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `api` (
 
 CREATE TABLE IF NOT EXISTS `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` int(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
@@ -60,3 +61,29 @@ CREATE TABLE IF NOT EXISTS `place` (
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+--
+-- Category inserts 
+--
+
+INSERT INTO category (name) VALUES ('Food'), ('Hangouts'), ('Leisure'), ('Personal care'), ('Religion'), ('Shopping');
+
+
+--
+-- Test inserts 
+--
+
+INSERT INTO place (name, address, categoryid, description, lat, lng, image) VALUES ('Mesón Casa Pedro', 'Calle de Asturias, 39, 33004, Oviedo', 1, 'Descripción de Mesón Casa Pedro', 43.364256, -5.856516, 'http://mesoncasapedro.es/components/com_fpss/images/cachopo2.jpg');
+
+INSERT INTO place (name, address, categoryid, description, lat, lng, image) VALUES ('Confitería Cafetería Santa Cristina', 'Calle Uría, 43, 33003, Oviedo', 2, 'Descripción de Cafetería Santa Cristina', 43.365861, -5.854309, 'http://www.pasteleriasantacristina.es/images/Fachada-2.jpg');
+
+INSERT INTO place (name, address, categoryid, description, lat, lng, image) VALUES ('Cines Los Prados', 'Avenida Fernández Ladreda, s/n, 33011, Oviedo', 3, 'Descripción de Cines Los Prados', 43.370894, -5.831764, 'http://www.yelmocines.es/sites/default/files/styles/cinema_gallery_full/public/p8030518.jpg');
+
+INSERT INTO place (name, address, categoryid, description, lat, lng, image) VALUES ('Go Fit Oviedo', 'Calle Asturcón, 23, 33006, Oviedo', 4, 'Descripción de gimnasioo Go Fit Oviedo', 43.352958, -5.861082, 'http://www.yelmocines.es/sites/default/files/styles/cinema_gallery_full/public/p8030518.jpg');
+
+INSERT INTO place (name, address, categoryid, description, lat, lng, image) VALUES ('Catedral de Oviedo', 'Plaza de Alfonso II El Casto, 33003, Oviedo', 5, 'Descripción de Catedral de Oviedo', 43.362493, -5.843607, 'http://taxicaminosantiago.com/wp-content/uploads/2013/04/Etapa1-camino-primitivo-2');
+
+INSERT INTO place (name, address, categoryid, description, lat, lng, image) VALUES ('Centro Comercial Modoo', 'Calle Arturo Álvarez Buylla, 5, 33005, Oviedo', 6, 'Descripción de Centro Comercial Modoo', 43.358466, -5.86089, 'http://esphoto980x880.mnstatic.com/centro-comercial-modoo_5700641.jpg');
+
+
+
