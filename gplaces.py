bcf381d553aeeee4ec889dea2f612e4cc18f85f2
Marco Vidal García <marcovidal67@gmail.com>
2015-02-10 22:51:40 +0100

    Google Places script changes to be Python3 compatible

--- gplaces.py
+++ gplaces.py
@@ -4,13 +4,13 @@ import json
 
 gplaces_api_key = 'AIzaSyAdZKWV1F9NBWVSk7YbCyf-7_NuM7jmFf8';
 
-response = urlopen('https://maps.googleapis.com/maps/api/place/textsearch/json?query=pizza+en+oviedo&sensor=false&key='+gplaces_api_key)
+response = urlopen('https://maps.googleapis.com/maps/api/place/textsearch/json?query=gimnasio+en+oviedo&sensor=false&key='+gplaces_api_key)
 #response = requests.get('https://maps.googleapis.com/maps/api/place/photo?photoreference=CnRnAAAABpkwwZCp-qL2et8-j2p1I8R7FbAM_Q9YY_F4t8-VDWRCGncE66rNSOVF2u9XvDMQIJDNI-pFMOp6nLKYjOZdV4yHGDHedQBilWPmycNB8OQUzOr4yhwK2lazxsh2dzstYHnHxP7q-7hzJZPok85dGBIQhKARrqiqqYw77AxA2utvqxoUj77ajoCp3NdgDtUN9ERIbhSPCYY&maxwidth=1600&sensor=false&key='+gplaces_api_key)
-
-data = response.read()
-#data = json.load(data)
+print(response.read().decode('utf-8'))
+#obj = json.loads(response.decode('utf-8'))
+#data = response.read()
 
 #fp = open('image.png', 'wb')
 #fp.write(response.content)
 #fp.close
-print(data)
+#print(obj)
